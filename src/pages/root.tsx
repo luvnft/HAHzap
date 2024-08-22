f63847e48aa1f90141d24c755a47d54276655b99
The Wizard of Hahz <info@luvnft.com>
2024-08-22 16:02:33 -0400

    updated

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -11,23 +11,23 @@ export function RootPage() {
   const { proxy } = useImgProxy();
   const shirtSize = 120;
   return (
-    <div className="flex flex-col gap-6 p-4 min-w-0">
-      <div className="flex max-md:flex-col gap-2 items-center bg-layer-2 p-4 text-lg font-medium text-pretty">
+    <div className="flex flex-col min-w-0 gap-6 p-4">
+      <div className="flex items-center gap-2 p-4 text-lg font-medium max-md:flex-col bg-layer-2 text-pretty">
         <img
           width={shirtSize}
           className="rounded-xl"
           src={proxy(
-            "https://blossom.nogood.studio/f98bc742ba24b2c729420148d736c3c0f58e6551d7dc0e4bd263d78bf2ab58b8.png",
+            "https://i.imgur.com/exy8v3c.jpeg",
             shirtSize,
             "f98bc742ba24b2c729420148d736c3c0f58e6551d7dc0e4bd263d78bf2ab58b8",
           )}
         />
         <FormattedMessage
-          defaultMessage="Visit {link} to get some sweet zap.stream merch!"
+          defaultMessage="Visit {link} to get a Z stream boost paid in Bitcoin!"
           values={{
             link: (
-              <Link to="https://store.zap.stream" className="underline" target="_blank">
-                store.zap.stream
+              <Link to="https://market.arvrtise.com" className="underline" target="_blank">
+                market.arvrtise.com
               </Link>
             ),
           }}
